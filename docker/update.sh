da67832177d2ebd96c50be59f86f4ad72dc258b9
Tim O'Donnell <timodonnell@gmail.com>
2023-05-16 13:40:56 -0400

    fix

--- docker/update.sh
+++ docker/update.sh
@@ -2,6 +2,7 @@
 
 # Run this inside the container to update repos
 PIP=/root/miniconda3/envs/design-env/bin/pip
+PYTHON=/root/miniconda3/envs/design-env/bin/python
 
 $PIP install -r /root/proteopt/requirements.txt
 $PIP install -e /root/proteopt
@@ -11,7 +12,16 @@ do
     cd ~/$i
     git fetch origin
     git reset --hard origin/main
-    $PIP install -e .
-    cd ..
 done
+
+cd ~/RFDiffusion/env/SE3Transformer
+$PIP install --no-cache-dir -r requirements.txt
+$PYTHON setup.py install
+
+for i in ProteinMPNN RFDesign RFDiffusion
+do
+    cd ~/$i
+    $PIP install -e .
+done
+
 $PIP install --upgrade biopython
